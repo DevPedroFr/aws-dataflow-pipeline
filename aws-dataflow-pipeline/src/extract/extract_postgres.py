b220646d511e0a426230766ea00dcfe33538f920
DevPedroFr <pedroks.09.04.07@gmail.com>
2025-06-23 14:49:43 -0300

    Extração de dados do PostgreSQL Finalizada

---
+++ aws-dataflow-pipeline/src/extract/extract_postgres.py
@@ -0,0 +1,30 @@
+import psycopg2
+import pandas as pd
+
+def extract_data_from_postgres():
+    try:
+        #configurações de conexão
+        conn = psycopg2.connect(
+            host ="localhost",
+            port ="5432",
+            database ="meu_banco",
+            user ="postgres",
+            password ="123456"    
+            )
+        
+        query = "SELECT * FROM vendas;"
+        df = pd.read_sql_query(query, conn)
+
+        print("Dados extraídos com sucesso")
+        print(df)
+
+        df.to_csv('vendas.csv', index=False)
+
+        conn.close()
+    except Exception as e:
+        print(f"Erro ao conectar ou extrair dados: {e}")
+
+if __name__ == "__main__":
+    extract_data_from_postgres()
+
+        
